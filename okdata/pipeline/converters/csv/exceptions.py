6d901d13258896d8421b4650378b7ec1fce9c6fd
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2020-12-07 13:40:53 +0100

    Remove superfluous `__init__`

--- okdata/pipeline/converters/csv/exceptions.py
+++ okdata/pipeline/converters/csv/exceptions.py
@@ -1,7 +1,4 @@
 class ConversionError(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
-
     @classmethod
     def from_value_error(cls, ve: ValueError):
         return cls("Cannot convert data according to json schema: " + str(ve))
