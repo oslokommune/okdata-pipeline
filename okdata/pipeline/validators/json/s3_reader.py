5cbb24ab1cd0a4a3519377f27f26a00c1e376a5e
Jon-Morten Kristiansen <jon.morten.kristiansen@origo.oslo.kommune.no>
2021-07-09 11:24:00 +0200

    DP-2162 Change param name

--- okdata/pipeline/validators/json/s3_reader.py
+++ okdata/pipeline/validators/json/s3_reader.py
@@ -6,8 +6,8 @@ s3 = boto3.client("s3")
 BUCKET = os.environ["BUCKET_NAME"]
 
 
-def read_s3_data(s3_input_prefix: dict) -> str:
-    prefix = next(iter(s3_input_prefix.values()))
+def read_s3_data(s3_input_prefixes: dict) -> str:
+    prefix = next(iter(s3_input_prefixes.values()))
 
     objects = s3.list_objects_v2(Bucket=BUCKET, Prefix=prefix)
     s3_path = next(iter(objects["Contents"]))["Key"]
