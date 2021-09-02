9b7845076197c1337c4795323275ae7c1a0d4721
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-09-02 10:13:21 +0200

    DP-2231 Support decoding multi-byte .csv.gz files
    UTF-8 code points can be multi-byte, so we need to take care not to
    decode chunks that happen to be split in the middle of a code point
    during gzip extraction.

--- okdata/pipeline/validators/csv/string_reader.py
+++ okdata/pipeline/validators/csv/string_reader.py
@@ -8,28 +8,33 @@ def from_response(raw_response, gzipped=False):
         # Plus 32 to automatically accept either the zlib or gzip format:
         # https://docs.python.org/3/library/zlib.html#zlib.decompress
         decompressor = zlib.decompressobj(wbits=32 + zlib.MAX_WBITS)
-        rest = ""
+        rest = b""
 
         for chunk in body.iter_chunks():
-            data = rest + decompressor.decompress(chunk).decode(encoding="utf-8")
-            lines = data.split("\n")
+            data = rest + decompressor.decompress(chunk)
+            lines = data.split(b"\n")
 
-            # We don't know yet whether the last element in the list is the
-            # beginning of a new row or if it is the last row in the file (in
-            # case the file doesn't have a final newline). Save it to be
-            # prepended to the next decoded chunk (or yielded if it was the
-            # last line).
+            # Save the last "line" to be prepended to the next chunk (or
+            # yielded if it was the last line) to cover two cases:
+            #
+            # 1. We don't know yet whether the last element in the list is the
+            #    beginning of a new row or if it is the last row in the file
+            #    (in case the file doesn't have a final newline).
+            #
+            # 2. Since UTF-8 code points can be multi-byte, we risk splitting
+            #    the stream in the middle of a code point, making the last part
+            #    incomplete and undecodable.
             rest = lines.pop()
 
             for line in lines:
-                yield line
+                yield line.decode("utf-8")
 
         if rest:
             # `rest` will contain the last line when the file didn't have a
             # final newline.
-            yield rest
+            yield rest.decode("utf-8")
 
     else:
         for line in body.iter_lines():
             if line:
-                yield line.decode(encoding="utf-8")
+                yield line.decode("utf-8")
